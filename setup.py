f88be74a46edb59f8fdb37648d23071bf6fa8809
Prakash Bhasker <pbbhaske@amazon.com>
2017-05-03 10:10:01 -0700

    Adds setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(
+    name='myapp',    # This is the name of your PyPI-package.
+    version='0.1',
+    packages=['myapp']    # Update the version number for new releases
+)
